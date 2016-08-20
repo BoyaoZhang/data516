9b51a0d51d36c6dd14fbbffc511b3f6df63505d2
BoyaoZhang <mill.zhang2010@gmail.com>
2016-08-20 21:06:38 +0200

    readData

---
+++ R/readData.R
@@ -0,0 +1,22 @@
+#' @title Read data
+#'
+#' @description Read data for all formats.
+#'
+#' @param file [\code{character(1)}]\cr
+#'   Give the file name with extenstions.
+#'
+#' @importFrom tools file_ext
+#' @importFrom readr read_csv
+#' @importFrom utils read.csv
+#' @export
+
+readData = function(file) {
+  ext = tools::file_ext(file)
+
+  if (ext == "csv")
+    # large file
+    if (file.size(file) > 10240000) # 10Mb
+      output = readr::read_csv(file)
+    else
+      output = read.csv(file)
+}
